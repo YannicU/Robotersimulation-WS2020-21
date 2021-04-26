a3375914858911f5da48447e45fce37c12a779b7
Yannic Yu <yannic.yue@gmail.com>
2021-04-26 22:23:27 +0200

    wer das liest ist doof

--- Punkt.java
+++ Punkt.java
@@ -1,38 +1,38 @@
 /**
  * Beschreiben Sie hier die Klasse Punkt.
- * 
+ *
  * @author Yannic Yu
  * @version V1 25.04.2021
  */
-public class Punkt 
-{
+public class Punkt {
     private int x = 0;
     private int y = 0;
 
     Punkt(){}
-
     Punkt (int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public int getX(){return x;}
-    public int getY(){return y;}
-
-    public void setXY(int neuX, int neuY){
-        this.x = neuX;
-        this.y = neuY;
-    }
-    public String ausgabeAttribute() {
-       return "x: " + getX() + " | y: " + getY();
-    }
-    public void bewegeUm(int dx, int dy){
-        this.x += dx;
-        this.y += dy;
-    }
+    public int getX(){return this.x;}
+    public int getY(){return this.y;}
 
     public double getAbstand(Punkt andererPunkt) {
         return Math.sqrt(Math.pow((andererPunkt.getX() - getX()), 2)
                 + Math.pow((andererPunkt.getY() - getY()), 2));
     }
+
+    public void setXY(int neuX, int neuY) {
+        this.x = neuX;
+        this.y = neuY;
+    }
+    public void bewegeUm(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public void ausgabeAttribute() {
+        System.out.println("Koordinaten des Punktes: x = " + getX() + ", y = " + getY());
+    }
+
 }
--- Rechteck.java
+++ Rechteck.java
@@ -1,7 +1,6 @@
-
 /**
  * Beschreiben Sie hier die Klasse Rechteck.
- * 
+ *
  * @author Yannic Yu
  * @version V1 26.04.2021
  */
@@ -11,39 +10,48 @@ public class Rechteck {
     private Punkt position = new Punkt(0, 0);
     private int breite = 0;
     private int laenge = 0;
-    private String name;
-    private Color farbe = Color.green;
-
+    private String bezeichnung;
+    private Color farbe;
+    /*
+     * Hier folgen die Konstruktoren
+     */
     Rechteck(){}
-    Rechteck(Punkt position, int breite, int laenge, String name, Color farbe) {
+    Rechteck(Punkt position, int breite, int laenge, String bezeichnung, Color farbe) {
         this.position = position;
         this.breite = breite;
         this.laenge = laenge;
-        this.name = name;
-        this.farbe = farbe;
+        this.bezeichnung = bezeichnung;
+        checkFarbe(farbe);
     }
-
+    /*
+     * Hier folgen die "get" Methoden
+     */
     public int getPositionX() {
-        return position.getX();
+        return this.position.getX();
     }
     public int getPositionY() {
-        return position.getY();
+        return this.position.getY();
     }
     public int getBreite() {
-        return breite;
+        return this.breite;
     }
     public int getLaenge() {
-        return laenge;
+        return this.laenge;
     }
-    public String getName() {
-        return name;
+    public String getBezeichnung() {
+        return this.bezeichnung;
     }
     public Color getColor() {
-        return farbe;
+        return this.farbe;
     }
-
+    /*
+     * Hier folgen die "set" Methoden
+     */
     public void setPosition(Punkt position) {
         this.position = position;
+    }
+    public void setPosition(int x, int y) {
+        this.position.setXY(x, y);
     }
     public void setBreite(int breite) {
         this.breite = breite;
@@ -51,25 +59,36 @@ public class Rechteck {
     public void setLaenge(int laenge) {
         this.laenge = laenge;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
     }
     public void setColor(Color farbe) {
-        this.farbe = farbe;
+        checkFarbe(farbe);
     }
 
-    public void bewegeUm(int dx, int dy){
+    private void checkFarbe(Color farbe) {
+        // überprüft ob die eingegebene Farbe weiß ist.
+        // Falls ja, wird keine Farbe generiert und eine Fehlermeldung an die Konsole gegeben.
+        if (farbe == Color.white) {
+            System.out.println("Farbe darf nicht weiß sein!");
+            this.farbe = null;
+        } else {
+            this.farbe = farbe;
+        }
+    }
+
+    public void bewegeUm(int dx, int dy) {
         this.position.bewegeUm(dx, dy);
     }
-    public void bewegeUm(Punkt verschiebevektor){
+    public void bewegeUm(Punkt verschiebevektor) {
         this.position.bewegeUm(verschiebevektor.getX(), verschiebevektor.getY());
     }
 
-    public String ausgabeAttribute(){
-        return "    Position: " + position.ausgabeAttribute() +
-                "\n    Breite: " + getBreite() + " px" +
-                "\n    Länge: " + getLaenge() + " px" +
-                "\n    Name: " + getName() +
-                "\n    Color: " + getColor() + "\n";
+    public void ausgabeAttribute() {
+        System.out.println("\nPosition: x = " + getPositionX() + ", y = " + getPositionY() +
+                "\nBreite: " + getBreite() + " px" +
+                "\nLänge: " + getLaenge() + " px" +
+                "\nBezeichnung: " + getBezeichnung() +
+                "\nColor: " + getColor());
     }
 }
